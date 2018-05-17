f0574708f9364a2acf8ce5c03a8dab0b8ce78a52
m305-9\w401 <A106082014@mail.shu.du.tw>
2018-05-17 18:06:15 +0800

    no open

---
+++ todolist/todolist/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace todolist
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // 產生一個方塊
+            TodoItem item = new TodoItem();
+
+            // 放到TodoList
+            TodoList.Children.Add(item);
+        }
+
+        private void Button_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // 產生一個儲存每個 list 文字的 list
+            List<string> all = new List<string>();
+
+            // 取得每一個 item 的文字，放到 list
+            foreach (TodoItem item in TodoList.Children)
+            {
+                all.Add(item.GetTaskName());
+            }
+
+            // 寫入檔案
+            System.IO.File.WriteAllLines(@"c:\tododata.txt", all);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ todolist/todolist/TodoItem.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace todolist
+{
+    /// <summary>
+    /// TodoItem.xaml 的互動邏輯
+    /// </summary>
+    public partial class TodoItem : UserControl
+    {
+        public TodoItem()
+        {
+            InitializeComponent();
+        }
+
+        public string GetTaskName()
+        {
+            return TaskName.Text;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
